5520c4930c5cf226f80680f492e6d0fd444f99d8
Alissa Troiano <alissatroianonyc@gmail.com>
2022-05-16 10:36:10 -0400

    Begin to add new elements for tiles

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # threejs
 three js animation for website redesign
+
+
+# CREDITS
+
+- (Getting Started With Three.js)[https://www.youtube.com/watch?v=8jP4xpga6yY]
+- (How To Make An Object Rotate Around Another Object In Three.js - Create A Solar System)[https://www.youtube.com/watch?v=XXzqSAt3UIw]
+- 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, player, texture, material;
 
 function init() {
   scene = new THREE.Scene();
@@ -16,19 +16,39 @@ function init() {
 
   var geometry = new THREE.BoxGeometry(4.5, 2, 2);
   // var material = new THREE.MeshBasicMaterial({ color: 0xd2d6d6 });
-  const texture = new THREE.TextureLoader().load('images/texture.png');
-  const material = new THREE.MeshBasicMaterial({ map: texture });
+  texture = new THREE.TextureLoader().load('images/texture.png');
+  material = new THREE.MeshBasicMaterial({ map: texture });
   cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
-
-  camera.position.z = 5;
+  camera.position.set(0, 0, 5);
 }
+
+// function tileDisplay() {
+//   scene = new THREE.Scene();
+//   camera = new THREE.PerspectiveCamera(
+//     75, 
+//     window.innerWidth / window.innerHeight, 
+//     0.1,
+//     1000
+//   );
+
+//   renderer = new THREE.WebGLRenderer({ antialias: true });
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+
+//   document.body.appendChild(renderer.domElement);
+//   var geometry = new THREE.BoxGeometry(2, 2, 2);
+//   texture = new THREE.TextureLoader().load('images/image.png');
+//   material = new THREE.MeshBasicMaterial({ map: texture });
+
+//   player = new THREE.Mesh(geometry, material);
+//   scene.add(player);
+//   camera.position.z = 2;
+// }
 
 function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.00;
+  cube.rotation.y += 0.00;
 
   renderer.render(scene, camera);
 }
@@ -43,3 +63,4 @@ window.addEventListener('resize', onWindowResize, false);
 
 init();
 animate();
+tileDisplay();
